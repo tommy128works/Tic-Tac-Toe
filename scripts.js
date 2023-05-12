f589bb35231fe6043a68b26e54f6aaa5ede81030
Tommy <tommy128works@gmail.com>
2023-05-11 20:22:35 -0400

    chore: create new branch for refactor and feature
    Working on placing markers for each player.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 let gameBoard = [
-  [1, 1, 0],
   [null, null, null],
-  [0, 1, 1],
+  [null, null, null],
+  [null, null, null],
 ];
 
 function updateBoard() {
-  let board = document.getElementById("gameBoard");
+  const board = document.getElementById("gameBoard");
 
   while (board.hasChildNodes()) {
     board.removeChild(board.firstChild);
@@ -13,17 +13,41 @@ function updateBoard() {
 
   for (let y = 0; y < 3; y++) {
     for (let x = 0; x < 3; x++) {
-      let tile = document.createElement("div");
+      const tile = document.createElement("div");
       if (gameBoard[y][x] === 1) {
         tile.textContent = "X";
+        tile.dataset.isFree = 0;
       } else if (gameBoard[y][x] === 0) {
         tile.textContent = "O";
+        tile.dataset.isFree = 0;
       } else {
         tile.textContent = "";
+        tile.dataset.isFree = 1;
       }
+      tile.addEventListener("click");
+      // where should i put the click function
+      // updates array with player's marker
+
       board.appendChild(tile);
     }
   }
 }
+
+const Player = (marker) => {
+  const getMarker = () => marker;
+
+  return { getMarker };
+};
+
+const playerOne = Player("X");
+const playerTwo = Player("O");
+
+// add to tile div "onclick" for free spaces if data-isFree = 1
+// data-isFree = 0 if taken
+
+// update array
+// assign 1 or 0 depending on which player's turn it is
+
+// update board
 
 updateBoard();
