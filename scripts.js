3df624187bc58120de02d0d2ad6b439bf677e0fb
Tommy <tommy128works@gmail.com>
2023-05-12 17:46:44 -0400

    feat: add functions for placing markers
    Functions to keep track of player turns and placing markers were
    completed. Markers can only be placed in free spaces.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,39 @@
 const gameController = (() => {
-  let gameBoard = [
-    // [null, null, null],
-    // [null, null, null],
-    // [null, null, null],
-    ["X", "X", "X"],
-    ["O", "O", "O"],
-    ["X", "O", "X"],
+  const gameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+    // ["X", "X", "X"],
+    // ["O", "O", "O"],
+    // ["X", "O", "X"],
   ];
+  let isPlayerOneTurn = true;
+
   // function getGameBoard
   const getGameBoard = () => gameBoard;
 
-  // function resetGame
+  // function changePlayerTurn
+  const changePlayerTurn = () => {
+    isPlayerOneTurn = !isPlayerOneTurn;
+  };
+
+  // function resetPlayerTurn
+  const resetPlayerTurn = () => {
+    isPlayerOneTurn = true;
+  };
 
   // function getPlayerTurn
+  const getPlayerTurn = () => isPlayerOneTurn;
 
   // function addMarker to array
-  const addMarker = (player, row, col) => {
-    gameBoard[col][row] = player.getMarker();
+  const addMarker = (marker, row, col) => {
+    gameBoard[row][col] = marker;
   };
+
   // function checkWinCond
-  return { getGameBoard };
+  // function resetGame
+
+  return { getGameBoard, changePlayerTurn, getPlayerTurn, addMarker };
 })();
 
 const displayController = (() => {
@@ -45,14 +59,25 @@ const displayController = (() => {
           tile.textContent = tempGameBoard[y][x];
         }
 
-        // tile.addEventListener("click", (event) => {
-        // need to change this to keep track of player turn
-        //   addMarker(
-        //     player,
-        //     event.target.getAttribute("data-row"),
-        //     event.target.getAttribute("data-col")
-        //   );
-        // });
+        tile.addEventListener("click", (event) => {
+          if (event.target.getAttribute("data-is-free") == 1) {
+            if (gameController.getPlayerTurn() === true) {
+              gameController.addMarker(
+                playerOne.getMarker(),
+                event.target.getAttribute("data-row"),
+                event.target.getAttribute("data-col")
+              );
+            } else {
+              gameController.addMarker(
+                playerTwo.getMarker(),
+                event.target.getAttribute("data-row"),
+                event.target.getAttribute("data-col")
+              );
+            }
+            displayController.updateBoard();
+            gameController.changePlayerTurn();
+          }
+        });
 
         board.appendChild(tile);
       }
@@ -63,13 +88,12 @@ const displayController = (() => {
 
 const Player = (marker) => {
   const getMarker = () => marker;
-  // const getMarker = () => console.log(marker);
 
   return { getMarker };
 };
 
-// const playerOne = Player("X");
-// const playerTwo = Player("O");
+const playerOne = Player("X");
+const playerTwo = Player("O");
 
 // console.log(gameController.getGameBoard());
 // console.log(playerOne.getMarker());
